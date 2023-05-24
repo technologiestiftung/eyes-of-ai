918ab1794a059c775382cfa153b9d7b613754342
julizet <juliazet@gmx.de>
2023-05-24 18:18:59 +0200

    style Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,11 @@ import * as React from "react";
 
 export const Header = () => {
 	return (
-		<div className="header">
-			<h1 className="title">
+		<div className="flex sm:gap-5 flex-col items-center justify-center text-center pt-10">
+			<h1 className="text-5xl font-semibold pt-2">
 				Through the eyes of AI
 			</h1>
-			<p className="subline">
+			<p className="text-mdfont-mediumtext-xl text-center">
 				Hier findest du alle Informationen zu unserem Projekt. 
 			</p>
 		</div>
