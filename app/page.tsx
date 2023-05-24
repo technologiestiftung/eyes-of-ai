7af7d23ac12ffd0382924c613ba0fb975c76a244
julizet <juliazet@gmx.de>
2023-05-24 16:32:26 +0200

    remove imageURL param to make deploy possible

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ import ImageGrid from "../components/ImageGrid";
 import imagesData from "../images.json";
 
 export default function Page() {
-
-  // Extract the URLs from the imagesData array
-  const imageURLs = imagesData.map((image) => image.url);
   
   return (
     <div>
       <Header />  
-      <ImageGrid imageURLs={imageURLs} />
+      <ImageGrid />
       <p>Lorem ipsum...</p>
     </div>
   );
