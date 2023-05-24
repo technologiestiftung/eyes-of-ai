8d5e382443003577ab4856923e3dac918d68a512
julizet <juliazet@gmx.de>
2023-05-24 18:19:21 +0200

    add Footer placeholder

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const Footer = () => {
+	return (
+		<div className="flex flex-col items-center justify-center pt-10">
+			<h3 className="text-3xl font-semibold pt-2">
+				Impressum
+			</h3>
+		</div>
+	);
+};
