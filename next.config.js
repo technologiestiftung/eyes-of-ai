ffddeabf44b2ee1108a41f5652337ccdab550b97
ff6347 <hash@ff6347.email>
2023-05-28 16:02:32 +0200

    chore(config): Remove unwanted config

--- next.config.js
+++ next.config.js
@@ -2,30 +2,7 @@
 
 module.exports = {
 	reactStrictMode: false,
-	compress: false,
 	poweredByHeader: false,
-	generateEtags: true,
-	distDir: ".next",
-	httpAgentOptions: {
-		keepAlive: true,
-	},
-	devIndicators: {
-		buildActivity: true,
-		buildActivityPosition: "bottom-right",
-	},
-	images: {
-		loader: "custom",
-	},
-	// basePath: process.env.NODE_ENV === 'production' ? '/human-next/out' : '',
-	// assetPrefix: process.env.NODE_ENV === 'production' ? '/human-next/out/' : '',
-	exportPathMap: async function (
-		defaultPathMap,
-		{ dev, dir, outDir, distDir, buildId },
-	) {
-		return {
-			"/": { page: "/" },
-		};
-	},
 	webpack: (config, options) => {
 		config.module.rules.push({ test: /human.esm.js/, type: "javascript/esm" });
 		return config;
