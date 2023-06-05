4b08026a49a26801bf5d3766c6fd2da88535cd32
ff6347 <hash@ff6347.email>
2023-06-05 15:38:34 +0200

    feat(csrf): Use csrf tokens

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,50 @@
 "use client";
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import ImageGrid from "../components/ImageGrid";
 import styles from "../styles/elements.module.css";
 import RunHuman from "../components/RunHumanFn";
 import InitWebCam from "../components/InitWebCam";
-export default function Page() {
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import ApiExplorer from "./api-explorer";
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	const token = context.res.req.headers["x-csrf-token"] as string;
+
+	return { props: { csrf: token } };
+};
+
+const Page: React.FC<
+	InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ csrf }) => {
 	const videoRef = useRef<HTMLVideoElement | undefined>(undefined);
 	const canvasRef = useRef<HTMLCanvasElement | undefined>(undefined);
 
 	return (
 		<div>
 			<Header />
-			<canvas id="canvas"
-							ref={canvasRef}
-							className={styles.output}
-							onClick={() => videoRef.current?.paused ? videoRef.current?.play() : videoRef.current?.pause()}
+			<canvas
+				id="canvas"
+				ref={canvasRef}
+				className={styles.output}
+				onClick={() =>
+					videoRef.current?.paused
+						? videoRef.current?.play()
+						: videoRef.current?.pause()
+				}
 			/>{" "}
 			{/* placeholder element that will be used by human for output */}
-			<video id="video"
-						 ref={videoRef}
-						 className={styles.webcam}
-						 autoPlay
-						 muted
-						 onResize={() => {
-							 canvasRef.current!.width = videoRef.current!.videoWidth;
-							 canvasRef.current!.height = videoRef.current!.videoHeight;
-						 }}
+			<video
+				id="video"
+				ref={videoRef}
+				className={styles.webcam}
+				autoPlay
+				muted
+				onResize={() => {
+					canvasRef.current!.width = videoRef.current!.videoWidth;
+					canvasRef.current!.height = videoRef.current!.videoHeight;
+				}}
 			/>{" "}
 			{/* placeholder element that will be used by webcam */}
 			<div id="status" className={styles.status}></div>
@@ -38,8 +54,11 @@ export default function Page() {
 			{/* initialized webcam using htmlvideo element with specified id */}
 			<RunHuman videoRef={videoRef} canvasRef={canvasRef} />{" "}
 			{/* loads and start human using specified input video element and output canvas element */}
+			<ApiExplorer csrf={csrf} />
 			<ImageGrid />
 			<Footer />
 		</div>
 	);
-}
+};
+
+export default Page;
