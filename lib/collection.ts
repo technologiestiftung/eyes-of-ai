fdfaecb20a665e962a0e239d96e4a255f78908a6
julizet <juliazet@gmx.de>
2023-06-07 13:49:38 +0200

    add materials, styles, colors + function

--- lib/collection.ts
+++ lib/collection.ts
@@ -9,3 +9,53 @@ export class Collection {
 		return this.list[Math.floor(Math.random() * this.list.length)];
 	}
 }
+
+const materials = {
+	"dripping liquid oil painting": ["realistic", "modern"],
+	"retro-futuristic cinematic etching": ["synthwave"],
+	"inky watercolour painting": ["abstract", "art nouveau", "pre-raphaelite"], 
+	"expressive gouache illustration": ["surreal art"], // Salvador Dalí 
+	"lithography": ["pixel art", "distorted pixel art"], 
+	"sharp focused lithography": ["flat woodblock prints", "ornamental curvilinear lines"], 
+	"detailed pastel drawing": ["conceptual art"], // Craig Mullins
+	"ink drawing": ["digital art", "art deco"],
+	"purist acrylic painting": ["cubism"], 
+	"spiritual acrylic painting": ["folk art"]
+}
+
+const styles = {
+	"realistic": ["vibrant colours", "global lightning", "atmospheric vivid lightning"],
+	"synthwave": ["neon colours", "chromatic material", "geometrical, luminous lines", "colourful luminous lines"],
+	"abstract": ["colourful"], 
+	"art nouveau": ["vibrant splashes"],
+	"pre-raphaelite": ["vibrant colours"], 
+	"dark-vintage": ["vaporwave neon"], 
+	"surreal art": ["vaporwave neon", "vaporwave neon and clear shapes", "mind-bending shapes and colours"], 
+	"pixel art": ["monochrome"], 
+	"distorted pixel art": ["black and white"],
+	"ornamental curvilinear lines": ["black and white"],
+	"flat woodblock prints": ["black and white"],
+	"digital art": ["vibrant colors", "lgbtiq colours"],
+	"art deco": ["vibrant colors", "lgbtiq colours"], 
+	"cubism": ["raw light pastel colours", "contrast-rich and monochromatic"],
+	"folk art": ["symbolic colours", "saturated pigments", "eye-catching colour palette"]
+
+}
+
+//pleas write a function that first selects a random object key from materials, then get rendom style from material key and then gets a random color from style key
+
+export function getCollection(): { material: string, style: string, color: string } {
+	const materialKeys = Object.keys(materials);
+	const randomMaterialKey = materialKeys[Math.floor(Math.random() * materialKeys.length)];
+	const randomStyle = materials[randomMaterialKey][Math.floor(Math.random() * materials[randomMaterialKey].length)];
+  
+	const styleKeys = Object.keys(styles);
+	const randomStyleKey = styleKeys[Math.floor(Math.random() * styleKeys.length)];
+	const randomColor = styles[randomStyleKey][Math.floor(Math.random() * styles[randomStyleKey].length)];
+  
+	return {
+	  material: randomMaterialKey,
+	  style: randomStyle,
+	  color: randomColor
+	};
+  }
