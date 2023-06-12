7ed67c01c9a6249227d663c6e9dbf3a0cba341fb
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-06-12 15:31:47 +0200

    Fix prettier confusion

--- hooks/useDetectionText.ts
+++ hooks/useDetectionText.ts
@@ -2,35 +2,35 @@ import { Result } from "@vladmandic/human";
 import { useMemo } from "react";
 
 export interface DetectionText {
-  core: string;
-  emotion: string;
-  gesture: string;
+	core: string;
+	emotion: string;
+	gesture: string;
 }
 
 const useDetectionText = (result: Partial<Result>) => {
-  const detectionText = useMemo(() => {
-    if (!result || !result.face[0]) {
-      return undefined;
-    }
-    const face = result.face[0];
-    const distinctGestures = result.gesture
-      .map(({ gesture }) => gesture.toString())
-      .filter((x, i, a) => a.indexOf(x) == i);
+	const detectionText = useMemo(() => {
+		if (!result || !result.face[0]) {
+			return undefined;
+		}
+		const face = result.face[0];
+		const distinctGestures = result.gesture
+			.map(({ gesture }) => gesture.toString())
+			.filter((x, i, a) => a.indexOf(x) == i);
 
-    const coreLabel = `${Math.round(face.age)} years old ${face.gender} person`;
-    const emotionLabel = `${face.emotion
-      .map(({ emotion, score }) => `${Math.round(score * 100)}% ${emotion}`)
-      .join(", ")}`;
-    const gesturesLabel = `${distinctGestures.join(", ")}`;
+		const coreLabel = `${Math.round(face.age)} years old ${face.gender} person`;
+		const emotionLabel = `${face.emotion
+			.map(({ emotion, score }) => `${Math.round(score * 100)}% ${emotion}`)
+			.join(", ")}`;
+		const gesturesLabel = `${distinctGestures.join(", ")}`;
 
-    return {
-      core: coreLabel,
-      emotion: emotionLabel,
-      gesture: gesturesLabel,
-    } as DetectionText;
-  }, [result]);
+		return {
+			core: coreLabel,
+			emotion: emotionLabel,
+			gesture: gesturesLabel,
+		} as DetectionText;
+	}, [result]);
 
-  return { detectionText };
+	return { detectionText };
 };
 
 export default useDetectionText;
--- hooks/usePaginatedImages.ts
+++ hooks/usePaginatedImages.ts
@@ -5,30 +5,30 @@ import { anonClient } from "../lib/supabase";
 type Image = Database["public"]["Tables"]["eotai_images"]["Row"];
 
 const usePaginatedImages = () => {
-  const [isLoading, setIsLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(true);
 
-  const fetchPaginatedImages = useCallback(
-    async (
-      page: number,
-      pageSize: number,
-      callback: (imageData: Image[]) => void
-    ) => {
-      setIsLoading(true);
+	const fetchPaginatedImages = useCallback(
+		async (
+			page: number,
+			pageSize: number,
+			callback: (imageData: Image[]) => void
+		) => {
+			setIsLoading(true);
 
-      setIsLoading(true);
-      const { data, error } = await anonClient
-        .from("eotai_images")
-        .select("*")
-        .order("created_at", { ascending: false })
-        .range(page * pageSize, (page + 1) * pageSize - 1);
-      setIsLoading(false);
-      if (error) throw new Error(error.message);
-      callback(data);
-    },
-    []
-  );
+			setIsLoading(true);
+			const { data, error } = await anonClient
+				.from("eotai_images")
+				.select("*")
+				.order("created_at", { ascending: false })
+				.range(page * pageSize, (page + 1) * pageSize - 1);
+			setIsLoading(false);
+			if (error) throw new Error(error.message);
+			callback(data);
+		},
+		[]
+	);
 
-  return { fetchPaginatedImages, isLoading };
+	return { fetchPaginatedImages, isLoading };
 };
 
 export default usePaginatedImages;
