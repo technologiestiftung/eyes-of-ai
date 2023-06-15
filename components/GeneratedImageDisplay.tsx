ea367efa4a8d2702f69c88d0c8e5f751c26699be
Jonas Jaszkowic <jonas.jaszkowic@ts.berlin>
2023-06-15 11:20:28 +0200

    Prompt below generated image

--- components/GeneratedImageDisplay.tsx
+++ components/GeneratedImageDisplay.tsx
@@ -27,15 +27,15 @@ const GeneratedImageDisplay: React.FC<Props> = ({
 						width={window.innerWidth}
 						height={20}
 					></ProgressBar>
+					{generatedImageSrc && (
+						<div>
+							<img src={generatedImageSrc} alt={prompt.promptDe} />
+						</div>
+					)}
 					{prompt && <div style={{ width: "50%" }}>{prompt.promptDe}</div>}
 					{imageGenerationInProgress && (
 						<div style={{ width: "50%", fontSize: "large", padding: "20px" }}>
 							Interpretation der KI wird erstellt...
-						</div>
-					)}
-					{generatedImageSrc && (
-						<div>
-							<img src={generatedImageSrc} alt={prompt} />
 						</div>
 					)}
 				</div>
