13ac9fcb563c73957081791e61e957074943f326
ff6347 <hash@ff6347.email>
2023-06-15 14:27:42 +0200

    fix(migrations): Remove policy creation from mig

--- supabase/migrations/20230613160732_prompt_de.sql
+++ supabase/migrations/20230613160732_prompt_de.sql
@@ -1,12 +1,3 @@
-alter table "public"."eotai_images" add column "prompt_de" text;
-
-
-create policy "Public Access"
-on "storage"."objects"
-as permissive
-for select
-to public
-using ((bucket_id = 'eotai_images'::text));
-
-
+ALTER TABLE "public"."eotai_images"
+	ADD COLUMN "prompt_de" text;
 
