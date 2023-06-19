5f2a50f2ca378946ee8d4c6734bc0da1c0698b41
ff6347 <hash@ff6347.email>
2023-06-19 09:36:06 +0200

    style: Make canvas white

--- components/HumanDetectionDisplay.tsx
+++ components/HumanDetectionDisplay.tsx
@@ -36,7 +36,7 @@ const HumanDetectionDisplay: React.FC<Props> = ({
 		if (canvasRef.current && detectedHuman) {
 			var ctx = canvasRef.current.getContext("2d");
 			ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-			ctx.fillStyle = "#000000";
+			ctx.fillStyle = "white";
 			ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 			detectedHuman.draw.all(
 				canvasRef.current,
@@ -44,12 +44,12 @@ const HumanDetectionDisplay: React.FC<Props> = ({
 				{
 					color: "#F64C72",
 					roundRect: 0,
-					lineWidth: 2,
+					lineWidth: 5,
 					drawPolygons: true,
 					drawLabels: false,
 					drawBoxes: standStillDetected,
-					drawGaze: false,
-					drawPoints: false,
+					drawGaze: true,
+					drawPoints: true,
 					drawAttention: false,
 					drawGestures: false,
 				}
@@ -59,30 +59,36 @@ const HumanDetectionDisplay: React.FC<Props> = ({
 
 	return (
 		<>
-			<div className={styles.standStillHint}>
+			<div className="flex flex-col items-center content-start justify-center text-2xl text-center">
 				{!snapshotTriggered && standStillDetected ? (
 					<div>
 						<ProgressBar
 							progress={standStillProgress}
-							width={window.innerWidth}
+							width={512}
 							height={20}
 						></ProgressBar>
 						<div>Bleib so stehen!</div>
 					</div>
 				) : (
-					<div style={{ paddingTop: "20px" }}>Still stehen!</div>
+					<div className="text-center">Still stehen!</div>
+				)}
+				<canvas
+					id="canvas"
+					ref={canvasRef}
+					className="w-512px h-512px"
+					width="512"
+					height="512"
+				/>
+				{detectionText && (
+					<div className={styles.detectionText}>
+						<div>
+							<p>{detectionText.core}</p>
+							<p>{detectionText.emotion}</p>
+							<p>{detectionText.gesture}</p>
+						</div>
+					</div>
 				)}
 			</div>
-			<canvas id="canvas" ref={canvasRef} className={styles.output} />
-			{detectionText && (
-				<div className={styles.detectionText}>
-					<div>
-						<p>{detectionText.core}</p>
-						<p>{detectionText.emotion}</p>
-						<p>{detectionText.gesture}</p>
-					</div>
-				</div>
-			)}
 		</>
 	);
 };
