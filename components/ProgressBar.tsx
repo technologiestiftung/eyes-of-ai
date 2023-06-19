db44372e5d610e74688d683dfdf7ba5db4d7f3be
ff6347 <hash@ff6347.email>
2023-06-19 09:36:34 +0200

    style: Use tailwind

--- components/ProgressBar.tsx
+++ components/ProgressBar.tsx
@@ -10,10 +10,10 @@ const ProgressBar: React.FC<Props> = ({ progress, width, height }) => {
 	return (
 		<>
 			<div
+				className={"bg-white border-t-2 border-gray-400 border-dotted"}
 				style={{
 					width: progress * width,
 					height: height,
-					backgroundColor: "white",
 				}}
 			></div>
 		</>
